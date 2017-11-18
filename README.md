68b8dc75ba38aacb0b89c341799198deab1e0197
Chandan Raj Rupakheti <rupakhet@rose-hulman.edu>
2017-11-17 23:05:33 -0500

    Updated the project to use SOOT as the bytecode instrumention framework.

--- README.md
+++ README.md
@@ -1,10 +1,18 @@
 # Reverse-Engineered Design (RevEngD)
 
 [![build status](https://ada.csse.rose-hulman.edu/CSSE374-Public/RevEngD/badges/master/build.svg)](https://ada.csse.rose-hulman.edu/CSSE374-Public/RevEngD/commits/master)
-
 [![coverage report](https://ada.csse.rose-hulman.edu/CSSE374-Public/RevEngD/badges/master/coverage.svg)](https://ada.csse.rose-hulman.edu/CSSE374-Public/RevEngD/commits/master)
 
-This application reverse engineers the design of a supplied codebase.
+This is a starter repository for **Software Design - CSSE 374** term project. This application reverse engineers the design of a supplied codebase using [SOOT](https://github.com/Sable/soot), a bytecode instrumentation framework. You will need to learn a few things about SOOT to do a good job in the project. Use these references whenever you get stuck:
+1. [SOOT Survival Guide PDF](http://www.brics.dk/SootGuide/sootsurvivorsguide.pdf)
+2. [Fundamental Soot Objects](https://github.com/Sable/soot/wiki/Fundamental-Soot-objects)
+3. [Other Online Help](https://github.com/Sable/soot/wiki/Getting-help)
+
+## About Seed Contents
+The repo, as is, contains four examples for you to try out and expand. Lets' take a quick tour of the repo contents:
+
+1. SceneBuilder API -  
+
 
 To build this project from command line (note that it also computes test coverage):
 **./gradlew build**
---
+++ src/test/java/csse374/revengd/soot/SceneBuilderTest.java
@@ -0,0 +1,167 @@
+package csse374.revengd.soot;
+
+import static org.junit.Assert.*;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import csse374.revengd.soot.SceneBuilder;
+import soot.Scene;
+
+public class SceneBuilderTest {
+	SceneBuilder builder;
+	List<String> allFixtureClasses;
+	List<String> allRechableClassesFromEntry;
+	List<String> allUnRechableClassesFromEntry;
+
+	@Before
+	public void setUp() throws Exception {
+		this.builder = SceneBuilder.create();
+		
+		this.allRechableClassesFromEntry = Arrays.asList(
+				"csse374.revengd.examples.fixtures.CalculatorApp",
+				"csse374.revengd.examples.fixtures.CalculatorA",
+				"csse374.revengd.examples.fixtures.CalculatorB",
+				"csse374.revengd.examples.fixtures.ICalculator"
+		);
+		
+		this.allUnRechableClassesFromEntry = Arrays.asList(
+				"csse374.revengd.examples.fixtures.CalculatorC",
+				"csse374.revengd.examples.fixtures.UnrelatedClass"				
+		);
+		
+		this.allFixtureClasses = new ArrayList<>();
+		this.allFixtureClasses.addAll(this.allRechableClassesFromEntry);
+		this.allFixtureClasses.addAll(this.allUnRechableClassesFromEntry);
+
+		// Needed for running tests from Gradle
+		Path buildResourcesTestPath = Paths.get("build", "resources", "test");
+		if(!buildResourcesTestPath.toFile().exists()) {
+			buildResourcesTestPath.toFile().mkdirs();
+		}
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testCreate() {
+		assertNotNull(builder);
+	}
+
+	@Test
+	public void testAddClassPath() {
+		List<String> classPaths = Arrays.asList("abc", "def");		
+		classPaths.forEach(path -> builder.addClassPath(path));
+		
+		assertEquals(classPaths, builder.classPaths);
+	}
+
+	@Test
+	public void testAddClassPaths() {
+		List<String> classPaths = Arrays.asList("abc", "def");		
+		builder.addClassPaths(classPaths);
+		
+		assertEquals(classPaths, builder.classPaths);
+	}
+
+	@Test
+	public void testAddDirectory() {
+		List<String> dirs = Arrays.asList("abc", "def");		
+		dirs.forEach(dir -> builder.addDirectory(dir));
+		
+		assertEquals(dirs, builder.dirsToProcess);
+	}
+
+	@Test
+	public void testAddDirectories() {
+		List<String> dirs = Arrays.asList("abc", "def");		
+		builder.addDirectories(dirs);
+		
+		assertEquals(dirs, builder.dirsToProcess);
+	}
+
+	@Test
+	public void testSetEntryClass() {
+		String entryClass = "package.Class";
+
+		builder.setEntryClass(entryClass);
+		assertEquals(entryClass, builder.entryClassToLoad);
+	}
+
+	@Test
+	public void testSetEntryMethod() {
+		String entryMethod = "main";
+
+		builder.setEntryMethod(entryMethod);
+		assertEquals(entryMethod, builder.entryMethodToLoad);
+	}
+
+	@Test
+	public void testAddExclusion() {
+		List<String> exclusions = Arrays.asList("abc.*", "def.abc.*");
+		exclusions.forEach(exclusion -> builder.addExclusion(exclusion));
+		
+		assertEquals(exclusions, builder.exclusions);
+	}
+
+	@Test
+	public void testAddExclusions() {
+		List<String> exclusions = Arrays.asList("abc.*", "def.abc.*");
+		builder.addExclusions(exclusions);
+		
+		assertEquals(exclusions, builder.exclusions);
+	}
+	
+	@Test
+	public void testShouldLoadClassesOnlyRechableFromMain() {
+		final Scene scene = SceneBuilder.create()
+				.addExclusions(Arrays.asList("sun.*", "soot.*", "polygot.*"))
+				.setEntryClass("csse374.revengd.examples.fixtures.CalculatorApp")
+				.setEntryMethod("main")
+				.build();
+
+		this.allRechableClassesFromEntry.forEach(clazz -> {
+			assertTrue(scene.containsClass(clazz));
+		});
+
+		this.allUnRechableClassesFromEntry.forEach(clazz -> {
+			assertFalse(scene.containsClass(clazz));
+		});
+	}
+
+	@Test
+	public void testProcessDirShouldAnalyzeAllClassesInDir() {
+		Path path = Paths.get("build", "classes", "java", "test");
+		if(!path.toFile().exists()) {
+			path = Paths.get("bin");
+			
+			if(!path.toFile().exists()) {
+				fail("Could not find the classes directory - [" + path.toAbsolutePath().toString() + "]");
+			}
+		}
+		
+		String dirToProcess = path.toAbsolutePath().toString();
+		System.out.println("Processing test analysis from: " + dirToProcess);
+		
+		final Scene scene = SceneBuilder.create()
+				.addExclusions(Arrays.asList("sun.*", "soot.*", "polygot.*"))
+				.setEntryClass("csse374.revengd.examples.fixtures.CalculatorApp")
+				.setEntryMethod("main")
+				.addDirectory(path.toAbsolutePath().toString())
+				.build();
+
+		
+		this.allFixtureClasses.forEach(clazz -> {
+			assertTrue(scene.containsClass(clazz));
+		});
+	}
+}

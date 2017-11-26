178933e8206af51ff96dfad6114d80ba0559a9b3
Chandan Raj Rupakheti <rupakhet@rose-hulman.edu>
2017-11-25 22:39:39 -0500

    Added API to load a single or a collection of classes rather than just loading classes from a directory.

--- src/test/java/csse374/revengd/soot/SceneBuilderTest.java
+++ src/test/java/csse374/revengd/soot/SceneBuilderTest.java
@@ -16,6 +16,8 @@ import static org.mockito.Mockito.*;
 
 import csse374.revengd.soot.SceneBuilder;
 import soot.Scene;
+import soot.SootClass;
+import soot.SootMethod;
 
 public class SceneBuilderTest {
 	SceneBuilder builder;
@@ -75,6 +77,22 @@ public class SceneBuilderTest {
 		assertEquals(classPaths, builder.classPaths);
 	}
 
+	@Test
+	public void testAddClass() {
+		List<String> classes = Arrays.asList("abc", "def");		
+		classes.forEach(clazz -> builder.addClass(clazz));
+		
+		assertEquals(classes, builder.classes);
+	}
+
+	@Test
+	public void testAddClasses() {
+		List<String> classes = Arrays.asList("abc", "def");		
+		builder.addClasses(classes);
+		
+		assertEquals(classes, builder.classes);
+	}
+	
 	@Test
 	public void testAddDirectory() {
 		List<String> dirs = Arrays.asList("abc", "def");		
@@ -148,6 +166,62 @@ public class SceneBuilderTest {
 		});
 	}
 	
+	@Test
+	public void testShouldLoadClassesRechableFromMainAndManuallyLoadedClasses() {
+		final Scene scene = SceneBuilder.create()
+				.addExclusions(Arrays.asList("sun.*", "soot.*", "polygot.*"))
+				.setEntryClass("csse374.revengd.examples.fixtures.CalculatorApp")
+				.addClass("csse374.revengd.examples.fixtures.CalculatorC")
+				.addClass("csse374.revengd.examples.fixtures.UnrelatedClass")
+				.addEntryPointMatcher(new MainMethodMatcher("csse374.revengd.examples.fixtures.CalculatorApp"))
+				.build();
+
+		this.allFixtureClasses.forEach(clazz -> {
+			assertTrue(scene.containsClass(clazz));
+		});
+	}	
+
+	@Test
+	public void testShouldLoadClassesFromJDK() {
+		final String jComponentName = "javax.swing.JComponent";
+		final Scene scene = SceneBuilder.create()
+				.addExclusions(Arrays.asList("soot.*", "polygot.*"))
+				.setEntryClass("csse374.revengd.examples.fixtures.CalculatorApp")
+				.addClass(jComponentName)
+				.addEntryPointMatcher(new MainMethodMatcher("csse374.revengd.examples.fixtures.CalculatorApp"))
+				.build();
+
+		// Check if the JComponent is loaded successfully
+		SootClass jComponentClass = scene.getSootClass(jComponentName);
+		assertNotNull(jComponentClass);
+		
+		// Check if the "void paint(Graphics)" method is present
+		SootMethod method = jComponentClass.getMethodByName("paint");
+		assertTrue(method.isPublic());
+		assertEquals(method.getParameterCount(), 1);
+		
+		// Check if the body of the paint method is also loaded
+		assertNotNull(method.retrieveActiveBody());
+		
+		// Check if the hierarchy information is preserved
+		SootClass containerClass = jComponentClass.getSuperclass();
+		assertNotNull(containerClass);
+		
+		// Check if the "void getComponent(int)" method is present in the Container class
+		method = containerClass.getMethodByName("getComponent");
+		assertTrue(method.isPublic());
+		assertEquals(method.getParameterCount(), 1);
+				
+		// Check that the fields of the java.awt.Container class is also loaded
+		// Looks up the layoutMgr field of the Container class
+		assertNotNull(containerClass.getFieldByName("layoutMgr"));
+		
+		// Check that the super class of the super is also loaded correctly
+		assertEquals("java.awt.Component", containerClass.getSuperclass().getName());
+		
+		// At this point it would be safe to conclude that the JDK class can be loaded correctly!
+	}
+
 	@Test
 	public void testProcessDirShouldAnalyzeAllClassesInDir() {
 		Path path = Paths.get("build", "classes", "main");

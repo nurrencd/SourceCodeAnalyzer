0c8bb26f710d017f0256be5400c5f09e2f88abcb
abuagarwal <agarwaa@rose-hulman.edu>
2017-12-09 20:27:53 -0500

    First Meeting after Design review
    Worked on proprocessor

--- src/main/java/ourStuff/App.java
+++ src/main/java/ourStuff/App.java
@@ -1,10 +1,16 @@
 package ourStuff;
 
+import java.util.Collection;
+
 public class App {
 
 	public static void main(String[] args) {
 		String cmd = args[0];
 		String path = args[1];
+		
+		Preprocessor pre = new Preprocessor();
+		Collection<Analyzer> analyzerCollection = pre.makePileline(args);
+		
 	}
 
 }
--- src/main/java/ourStuff/Preprocessor.java
+++ src/main/java/ourStuff/Preprocessor.java
@@ -1,10 +1,47 @@
 package ourStuff;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Preprocessor {
 	
-	Collection<Analyzer> makePileline(String cmd){
-		return null;
+	Collection<Analyzer> makePileline(String[] args){
+		Map<String, String> config  = configGen(args);
+		Collection<Analyzer> listOfAnalyzers = new ArrayList<>();
+		listOfAnalyzers.add(new RecursiveAnalyzer());
+		if(config.containsKey("-u")){
+			listOfAnalyzers.add(new RelationshipAnalyzer());
+			CodeGenAnalyzer cGen = new CodeGenAnalyzer();
+			listOfAnalyzers.add(cGen);
+			if(config.containsKey("-f")){
+				String instruction = config.get("-f");
+				if(instruction.equals("public")){
+					cGen.addFilter(new PublicFilter());
+				}else if(instruction.equals("protected")){
+					cGen.addFilter(new ProtectedFilter());
+				}else if(instruction.equals("private")){
+					cGen.addFilter(new PrivateFilter());
+				}
+			}
+		}
+		
+		return listOfAnalyzers;
+	}
+	
+	Map<String, String> configGen(String[] args){
+		Map<String, String> config =  new HashMap<String, String>();
+		String flag = "";
+		for(String e : args){
+			if(e.startsWith("-")){
+				flag = e;
+				config.put(flag, ""); //For flags that don't have a filter/value associated with it
+										// E.g. -u For generating UML
+			}else{
+				config.put(flag, e);
+			}
+		}
+		return config;
 	}
 }

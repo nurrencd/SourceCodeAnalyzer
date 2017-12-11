80b2e9ddbcbeb9b14689bd07cb87c9d960977afe
abuagarwal <agarwaa@rose-hulman.edu>
2017-12-10 21:06:22 -0500

    Work in CodeGen
    CodeGen and parsing things worked on.

--- src/main/java/ourStuff/CodeGenAnalyzer.java
+++ src/main/java/ourStuff/CodeGenAnalyzer.java
@@ -3,6 +3,7 @@ package ourStuff;
 import java.util.ArrayList;
 import java.util.List;
 
+import ourStuff.Relationship.RelationshipType;
 import soot.SootClass;
 import soot.SootField;
 import soot.SootMethod;
@@ -16,11 +17,66 @@ public class CodeGenAnalyzer implements Analyzer {
 
 	@Override
 	public Data analyze(Data data) {
+		StringBuilder code = new StringBuilder();
+		code.append("@startuml \n");
+		for(SootClass c : data.classes){
+			if(filterClass(c)){
+				code.append(genString(c, data) + "\n");
+			}
+		}
+		code.append("@enduml");
+		System.out.println(code.toString());
 		return null;
+	}
+	
+	private String genString(SootClass c, Data data){
+		StringBuilder code = new StringBuilder();
+		code.append("class " + c.getName());
+		for(Relationship r : data.relationships){
+			if(r.from.equals(c)){
+				if(r.type == RelationshipType.INHERITANCE){
+					code.append(" extends " + r.to.getName());
+				}else if(r.type == RelationshipType.IMPLEMENTATION){
+					code.append(" implements " + r.to.getName());
+				}
+			}
+		} //Relations have been added
+		code.append("{ \n");
+		for(SootField f : c.getFields()){
+			if(f.isPublic()){
+				code.append("  + ");
+			}else if(f.isProtected()){
+				code.append("  # ");
+			}else{
+				code.append("  - ");
+			}
+			if(f.isStatic()){
+				code.append("{static} ");
+			}
+			code.append(f.getType().toString() + " ");
+			code.append(f.getName() + " \n");
+		}
+		for(SootMethod m : c.getMethods()){
+			if(m.isPublic()){
+				code.append("  + ");
+			}else if(m.isProtected()){
+				code.append("  # ");
+			}else{
+				code.append("  - ");
+			}
+			if(m.isStatic()){
+				code.append("{static} ");
+			}
+			if(m.isAbstract()){
+				code.append("{abstract} ");
+			}
+			code.append(m.getReturnType().toString() + " ");
+			code.append(m.getName() + " \n");
+		}
+		return code.toString();
 	}
 
 	private boolean filterClass(SootClass c) {
-		String code;
 		//if class gets filtered
 		for (Filter filter : this.filters) {
 			if (filter.ignore(c)) {

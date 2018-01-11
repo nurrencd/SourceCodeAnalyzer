9f5113e9a920b82f102d8f6f5e1d2c2defa82541
EricMoorman <moormaet@rose-hulman.edu>
2018-01-10 19:03:33 -0500

    tested with some functions

--- src/main/java/ourStuff/SequenceDiagramAnalyzer.java
+++ src/main/java/ourStuff/SequenceDiagramAnalyzer.java
@@ -1,8 +1,16 @@
 package ourStuff;
 
 import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
 
+import soot.Scene;
 import soot.SootMethod;
+import soot.Unit;
+import soot.UnitBox;
+import soot.ValueBox;
+import soot.jimple.toolkits.callgraph.CallGraph;
+import soot.jimple.toolkits.callgraph.Edge;
 
 public class SequenceDiagramAnalyzer extends AbstractAnalyzer {
 
@@ -12,9 +20,23 @@ public class SequenceDiagramAnalyzer extends AbstractAnalyzer {
 		return null;
 	}
 	
-	private Collection<String> recursiveBuilder(SootMethod m){
-		//TODO: Implement this
+	private String recursiveBuilder(SootMethod m, int depth, Scene scene){
+		if (depth > 5){
+			return "";
+		}
+		List<UnitBox> lines = m.getActiveBody().getAllUnitBoxes();
+		CallGraph cg = scene.getCallGraph();
+		Iterator<Edge> edges = cg.edgesOutOf(m);
+		
+		while (edges.hasNext()){
+			Edge e = edges.next();
+			SootMethod meth = e.getTgt().method();
+			e.getTgt().method().getDeclaringClass();
+			recursiveBuilder(meth, depth+1, scene);
+		}
+		
 		return null;
 	}
+
 
 }
